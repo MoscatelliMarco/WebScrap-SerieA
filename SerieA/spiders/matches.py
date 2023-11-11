27d329c04205445195063f3612b70a0eb4efb1e6
Marco Moscatelli <marco.moscatelli06@gmail.com>
2023-11-11 22:40:59 +0100

    now working on laptopn resolution and not table, the code is faster and fixed some latency errors

--- SerieA/spiders/matches.py
+++ SerieA/spiders/matches.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
@@ -34,12 +35,13 @@ class MatchesSpider(scrapy.Spider):
         driver = webdriver.Chrome(options=chrome_options)
 
         # Load page and wait for it to load
+        logging.info('Loading the page')
         driver.get("https://www.legaseriea.it/it/serie-a")
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//p[@class='p2 primary-400 medium ms-auto me-auto uppercase']")))
         time.sleep(.5)
 
         # Set a different window => needed later because the design of this window is more easy to scrape
-        driver.set_window_size(900, 900)
+        driver.set_window_size(1200, 900)
 
         # Calculate max day
         # If start_day == 1 that means that the season is finished so max_day is just a really large number
@@ -56,35 +58,40 @@ class MatchesSpider(scrapy.Spider):
             matches_btn = driver.find_elements(By.XPATH, "//div[@class='d-lg-none d-block ms-auto']/a[@class='hm-button-icon']")
 
             for i, btn in enumerate(matches_btn):
+                logging.info("Wait page load and remove popup")
                 WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='left d-flex align-content-around flex-wrap justify-content-center']")))
+                time.sleep(.5)
                 self.remove_popup(driver)
 
-                # Set a different window => needed later because the design of this window is more easy to scrape
-                driver.set_window_size(900, 900)
-                time.sleep(.3)
-
+                # Reach the day with the match to analyze
+                logging.info("Reach day")
                 self.reach_day(current_day, driver)
                 time.sleep(0.3)
 
                 # Find the location of the match button and go there
-                btn = driver.find_element(By.XPATH, f"(//div[@class='d-lg-none d-block ms-auto']/a[@class='hm-button-icon'])[{i+1}]")
-                driver.execute_script("arguments[0].scrollIntoView(true);", btn)
+                logging.info('Opening match link')
+                match_btn = driver.find_element(By.XPATH, f"(//div[@class='d-lg-none d-block ms-auto']/a[@class='hm-button-icon'])[{i+1}]")
+                driver.execute_script("arguments[0].scrollIntoView(true);", match_btn)
                 # Adjustment needed to put the button in the window
                 driver.execute_script("window.scrollBy(0, -250);")
+                driver.get(match_btn.get_attribute('href'))
 
+                # Element with all pages html
                 self.pages = {}
 
-                driver.get(btn.get_attribute('href'))
-
                 # Set a different window => needed later because the design of this window is more easy to scrape
-                driver.set_window_size(1200, 1000)
-                stats_btn = driver.find_element(By.XPATH, "(//div[@class='hm-nav-section']/ul/li)[5]")
-                stats_btn.click()
-
+                logging.info('Going into stats')
+                WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "(//div[@class='hm-nav-section']/ul/li)[5]")))
+                stats_btn = driver.find_element(By.XPATH, "(//div[@class='hm-nav-section']/ul/li/a)[5]")
+                driver.get(stats_btn.get_attribute('href'))
                 WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//a[@id='tab-general']")))
-                time.sleep(1)
+                time.sleep(.5)
+
+                # Make all animations instant
                 driver.execute_script("document.documentElement.style.scrollBehavior = 'auto';")
 
+                # Remove popup and log
+                logging.info("Removing popup and saving htmls of all data")
                 self.remove_popup(driver)
 
                 # Scrape all the general info
@@ -112,8 +119,6 @@ class MatchesSpider(scrapy.Spider):
                 WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "(//p[contains(text(), 'Tiri totali')])[1]")))
                 time.sleep(.2)
                 self.pages['shots'] = driver.page_source
-
-                time.sleep(.3)
 
                 self.parse_match(driver, current_day)
 
@@ -175,41 +180,64 @@ class MatchesSpider(scrapy.Spider):
         except TimeoutException:
             print("Timed out waiting for element to be present in the DOM")
 
-
     def reach_day(self, day_to_reach, driver):
+        # Create a Select object
+        select_input = driver.find_element(By.XPATH, "(//select)[1]")
+        select = Select(select_input)
 
-        current_day = self.find_curr_day(driver)
+        # Find value and select it
+        option = driver.find_element(By.XPATH, f"((//select)[1]/option[contains(text(), {day_to_reach})])[1]")
+        value = option.get_property('value')
+        select.select_by_value(value)
 
-        # If current_day == 1 after button is not on the screen so go on day 2 to find it
-        if current_day != 1:
-            back_btn = driver.find_element(By.XPATH, "(//i[contains(text(), 'chevron_right')]/../parent::*[@class='d-flex align-items-center d-lg-none mt-3 mb-3']/div)[1]")
-            after_btn = driver.find_element(By.XPATH, "(//i[contains(text(), 'chevron_left')]/../parent::*[@class='d-flex align-items-center d-lg-none mt-3 mb-3']/div)[2]")
-        else:
-            after_btn = driver.find_element(By.XPATH, "(//i[contains(text(), 'chevron_right')]/../parent::*[@class='d-flex align-items-center d-lg-none mt-3 mb-3']/div)")
-            after_btn.click()
-            time.sleep(.5)
-            after_btn = driver.find_element(By.XPATH, "(//i[contains(text(), 'chevron_right')]/../parent::*[@class='d-flex align-items-center d-lg-none mt-3 mb-3']/div)[1]")
-
-        unique_el = driver.find_element(By.XPATH, "(//p[@class='p4 dark-grey medium uppercase'])[2]").text
-
-        # While cycle to reach the right day
-        while current_day != day_to_reach:
-
-            if current_day < day_to_reach:
-                after_btn.click()
-
-            if current_day > day_to_reach:
-                back_btn.click()
-
-            time.sleep(.2)
-
-            # Cause of latency there might be some errors if I just change the value of current_day directly without scraping it directly from the website
-            current_day = self.find_curr_day(driver)
+        time.sleep(.5)
 
     def find_curr_day(self, driver):
         # Find the current day that is being analyzed
-        current_day = driver.find_element(By.XPATH, "//p[@class='p2 primary-400 medium ms-auto me-auto uppercase']").text
-        current_day = current_day.replace("° GIORNATA", "")
+        # Find the select input
+        select_input = driver.find_element(By.XPATH, f"(//select)[1]")
+        
+        # Create a Select object and then get the selected object
+        select = Select(select_input)
+        selected_option = select.first_selected_option
+
+        # Find the current day with a little bit of processing
+        current_day = selected_option.text
+        # Remove the last useless part: "° Giornata"
+        current_day = current_day[:-10]
+        print(selected_option.text)
+        print(current_day)
         current_day = int(current_day)
 
         return current_day
+
+
+    # def reach_day(self, day_to_reach, driver):
+
+    #     current_day = self.find_curr_day(driver)
+
+    #     # If current_day == 1 after button is not on the screen so go on day 2 to find it
+    #     if current_day != 1:
+    #         back_btn = driver.find_element(By.XPATH, "(//i[contains(text(), 'chevron_right')]/../parent::*[@class='d-flex align-items-center d-lg-none mt-3 mb-3']/div)[1]")
+    #         after_btn = driver.find_element(By.XPATH, "(//i[contains(text(), 'chevron_left')]/../parent::*[@class='d-flex align-items-center d-lg-none mt-3 mb-3']/div)[2]")
+    #     else:
+    #         after_btn = driver.find_element(By.XPATH, "(//i[contains(text(), 'chevron_right')]/../parent::*[@class='d-flex align-items-center d-lg-none mt-3 mb-3']/div)")
+    #         after_btn.click()
+    #         time.sleep(.5)
+    #         after_btn = driver.find_element(By.XPATH, "(//i[contains(text(), 'chevron_right')]/../parent::*[@class='d-flex align-items-center d-lg-none mt-3 mb-3']/div)[1]")
+
+    #     unique_el = driver.find_element(By.XPATH, "(//p[@class='p4 dark-grey medium uppercase'])[2]").text
+
+    #     # While cycle to reach the right day
+    #     while current_day != day_to_reach:
+
+    #         if current_day < day_to_reach:
+    #             after_btn.click()
+
+    #         if current_day > day_to_reach:
+    #             back_btn.click()
+
+    #         time.sleep(.15)
+
+    #         # Cause of latency there might be some errors if I just change the value of current_day directly without scraping it directly from the website
+    #         current_day = self.find_curr_day(driver)
